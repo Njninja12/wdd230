af01b9c8edc42d44a44b6d23b3f23f177808cd21
Njninja12 <noah.j.wright316@gmail.com>
2024-05-06 13:12:14 -0700

    responsive nav

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,13 @@
 document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
 
 document.getElementById("year").innerHTML = new Date().getFullYear()
+
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+});
+    
